775a8c415ed81a6f186a0d12bb2701b3e47fc472
xoshenayrocks <shenay.russell@gmail.com>
2019-12-18 18:44:37 -0500

    Encoded message deliverable

---
+++ Deliverable_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Deliverable_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your message?");
+            string input = Console.ReadLine();
+            string inputCap = input.ToUpper();
+           
+            string message = " ";
+            int checksum = 0; 
+             
+            foreach (char x in inputCap)
+                
+                {
+                 
+                int result = (int)x - 64;
+             
+                 message += result.ToString() + "-";
+                
+                checksum += (int)x;
+                    
+                }
+
+            Console.WriteLine("Your encoded message is" + message);
+
+            Console.WriteLine("Message checksum is " + checksum);
+        
+        }
+    }
+
+}
+
